c1ac0be16c35d58fad6b5102805df523f911c940
aisyahnadhira <aisyahaisyah725@gmail.com>
2022-03-22 13:45:29 +0700

    add image assets

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,8 +6,11 @@ class HomePage extends StatelessWidget {
   HomePage({ Key? key }) : super(key: key);
 
   final List<Item> items = [
-    Item(name: 'Sugar', price: 5000),
-    Item(name: 'Salt', price: 2000)
+    Item(name: 'Sugar', price: 5000, photo: 'assets/images/sugar.png'),
+    Item(name: 'Salt', price: 2000, photo: 'assets/images/salt.png'),
+    Item(name: 'Cheese', price: 8000, photo: 'assets/images/chesee.png'),
+    Item(name: 'Garlic', price: 7000, photo: 'assets/images/garlic.png'),
+    Item(name: 'Bread', price: 15000, photo: 'assets/images/bread.png'),
   ];
 
   @override
@@ -29,13 +32,17 @@ class HomePage extends StatelessWidget {
               
               },
               child: Container(
-                margin: EdgeInsets.all(8),
+                margin: EdgeInsets.all(20),
                 child: Row(
                   children: [
-                    Expanded(child: Text(item.name)),
+                    Expanded(child: Text(item.name, style: TextStyle(fontSize: 20,fontWeight: FontWeight.w300))),
                     Expanded(
                       child: 
                         Text(item.price.toString(),
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w300
+                        ),
                         textAlign: TextAlign.end,
                     )),
                   ],
--- lib/pages/item_page.dart
+++ lib/pages/item_page.dart
@@ -8,11 +8,19 @@ class ItemPage extends StatelessWidget {
   Widget build(BuildContext context) {
       final Item itemArgs = ModalRoute.of(context)!.settings.arguments as Item;
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text(itemArgs.name),
-      // ),
+      backgroundColor: Colors.grey,
       body: Center(
-        child: Text(itemArgs.name + ' with ' + itemArgs.price.toString()),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(itemArgs.name + ' with ' + itemArgs.price.toString(), style: TextStyle(fontSize: 25, color: Colors.white)),
+            Container(
+              height: 300,
+              width: 300,
+              child: Image.asset(itemArgs.photo)),
+          ],
+        ),
+
       ),
     );
   }
