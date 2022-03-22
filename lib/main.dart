656a7a869e93e63c8f4ee65a9a5a5b44f0e8e183
aisyahnadhira <aisyahaisyah725@gmail.com>
2022-03-22 12:34:40 +0700

    add home screen

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shopping/pages/home_page.dart';
+import 'package:flutter_shopping/pages/item_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shopping List',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/item': (context) => ItemPage(),
+      },
+    );
+  }
+}
