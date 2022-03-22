64b575ccffe29a29889438b65f22459f032a2524
aisyahnadhira <aisyahaisyah725@gmail.com>
2022-03-22 12:51:44 +0700

    add item page

--- lib/pages/item_page.dart
+++ lib/pages/item_page.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shopping/models/item.dart';
 
 class ItemPage extends StatelessWidget {
-  const ItemPage({ Key? key }) : super(key: key);
+  ItemPage({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+      final Item itemArgs = ModalRoute.of(context)!.settings.arguments as Item;
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text(itemArgs.name),
+      // ),
+      body: Center(
+        child: Text(itemArgs.name + ' with ' + itemArgs.price.toString()),
+      ),
     );
   }
 }
